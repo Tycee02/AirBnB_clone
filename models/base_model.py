84798606d9c97b81c37eeaa070c005982fe37dd3
Gildo Chauze <gildochauze@gmail.com>
2024-02-06 15:08:48 +0200

    Task 5 completed

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Base Model class"""
 
 from datetime import datetime
+import models
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
@@ -17,9 +18,23 @@ class BaseModel:
         Return:
                 None
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        # Create an Instance from a dictionary
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
+        if hasattr(self, "created_at") and type(self.created_at) is str:
+            self.created_at = datetime.strptime(kwargs["created_at"], time)
+        if hasattr(self, "updated_at") and type(self.created_at) is str:
+            self.created_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            # Create a new Instance
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """Return the Representation of Object"""
@@ -38,6 +53,7 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of
@@ -49,7 +65,10 @@ class BaseModel:
                 None
 
         """
+        # Copy the instance __dict__ to new dictionary
         obj_dict = self.__dict__.copy()
+        # Check if the created_at and update_at attrs exists and format the
+        # date to ISO format
         if "created_at" in obj_dict:
             obj_dict["created_at"] = obj_dict["created_at"].strftime(
                 "%Y-%m-%dT%H:%M:%S.%f"
@@ -60,4 +79,5 @@ class BaseModel:
             )
         obj_dict["__class__"] = self.__class__.__name__
 
+        # Return the a dictionary with the class name and all attrs
         return obj_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Module containing functions to serialize and deserialize JSON"""
+import json
+
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {
+    "Amenity": Amenity,
+    "BaseModel": BaseModel,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
+
+
+class FileStorage:
+    """Class to handle the operations of serialization and deserialization
+    of JSON
+    """
+
+    # Private class attributes
+    __file_path = "file.json"  # Path to JSON file
+    __objects = {}  # store all objects by <class nam>.id
+
+    def all(self):
+        """Method to returns all objects created"""
+        return self.__objects
+
+    def new(self, obj):
+        """Method to set new object to storage"""
+        if obj is not None:
+            # Create the key <obj class name>.id
+            key = obj.__class__.__name__ + "." + obj.id
+            # Save the object in the Dictionary
+            self.__objects[key] = obj
+
+    def save(self):
+        """Method to serialize and save a JSON to file"""
+        json_obj = {}
+        for key in self.__objects:
+            json_obj[key] = self.__objects[key].to_dict()
+
+        with open(self.__file_path, "w") as f:
+            json.dump(json_obj, f)
+
+    def reload(self):
+        """Method to deserialize JSON from file"""
+        try:
+            with open(self.__file_path, "r") as f:
+                json_obj = json.load(f)
+
+            for key in json_obj:
+                # Retrieve the class name from the object
+                obj_class = classes[json_obj[key]["__class__"]]
+                # Retrive the class Dictionary
+                obj_dict = json_obj[key]
+                # Create a new Instance of the class using the
+                # dictionary
+                obj = obj_class(**obj_dict)
+                # Add the object to the __objects
+                self.__objects[key] = obj
+
+        except Exception as e:
+            pass
