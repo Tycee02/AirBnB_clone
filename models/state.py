4b5016b7496efef374a7c433aeeeeac7e1620f5b
Tycee02 <wakilaminat@gmail.com>
2024-02-09 17:26:54 +0100

    classes fo state

---
+++ models/state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""state.py"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    The state class inherit from Basemodel
+    """
+
+    name = ''
+
+    def __init__.(self, *args, **kwargs)
+    """
+    Initialize the user with *args and **kwargs
+    """
+
+    super().__init__(*args, **kwargs)
+    if 'name' in kwargs:
+        self.name = kwargs['name']
+        
