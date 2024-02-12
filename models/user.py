ca083e1d65c2c5c7dcb7692375bfc097a6380b5a
Gildo Chauze <gildochauze@gmail.com>
2024-02-12 13:57:59 +0200

    Task 8. First User

--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """user.py"""
+
+
 from models.base_model import BaseModel
 
 
@@ -9,22 +11,13 @@ class User(BaseModel):
     and inherits from the BaseModel cass.
     """
 
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
         """
         Initialize the User with *args and **kwargs
         """
-
         super().__init__(*args, **kwargs)
-        if 'email' in kwargs:
-            self.email = kwargs['email']
-        if 'password' in kwargs:
-            self.password = kwargs['password']
-        if first_name in kwargs:
-            self.first_name = kwargs['first_name']
-        if last_name in kwargs:
-            self.last_name = kwargs['last_name']
